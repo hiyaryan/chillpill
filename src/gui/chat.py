7bfac8146ddf6faa62417e8df28abc65ac9f16b9
Ryan Meneses <rmenese1@asu.edu>
2023-05-09 14:28:54 -0700

    Add chat text field and message posting.

--- src/gui/chat.py
+++ src/gui/chat.py
@@ -1,11 +1,32 @@
 import flet as ft
 
 
+class Message:
+    def __init__(self, user, text):
+        self.user = user
+        self.text = text
+
+
 def main(page: ft.Page):
     page.title = "Chat"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.add()
+
+    chat = ft.Column()
+    new_message = ft.TextField()
+
+    def on_message(message: Message):
+        chat.controls.append(ft.Text(f"Me: {message.text}"))
+        page.update()
+
+    def send_click(e):
+        on_message(Message(page.session_id, new_message.value))
+        new_message.value = ""
+        page.update()
+
+    page.add(
+        chat,
+        ft.Row(controls=[new_message, ft.ElevatedButton("Send", on_click=send_click)]),
+    )
 
 
 def launch():
-    ft.app(target=main)
+    ft.app("chat", target=main)
